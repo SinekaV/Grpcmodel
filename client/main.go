1f9a12f4407c2ccf366ab7c3b24219238c769e27
SinekaV <sneehefty@gmail.com>
2023-08-30 16:22:02 +0530

    customer and createdat displayes from the client

---
+++ client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"log"
+
+	"grpcmodel/constants"
+	pb "grpcmodel/customer"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	conn, err := grpc.Dial(constants.Port, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewCustomerServiceClient(conn)
+	
+
+	response, err := client.CreateCustomer(context.Background(), &pb.CustomerRequest{
+		CustomerId: 320,
+		Firstname:  "Jsri",
+		Lastname:   "R",
+		BankId:     12345,
+		Balance:    100000,
+	})
+	if err != nil {
+		log.Fatalf("Failed to call: %v", err)
+	}
+
+	fmt.Printf("CustomerID: %d\nCreatedTime:%v\n", response.CustomerId,response.CreatedAt)
+}
